305024b6eff66975fad82505c21ae5dff81138aa
KenchiPa <kenchi6734@naver.com>
2023-02-18 21:10:21 +0900

    Entity 설정

---
+++ src/main/java/com/boot/board/config/JpaConfig.java
@@ -0,0 +1,18 @@
+package com.boot.board.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return () -> Optional.of("hrpark");  // Auditing 을 할때마다 이 이름이 들어간다고 설정.
+    }
+}
--- src/main/java/com/boot/board/domain/Article.java
+++ src/main/java/com/boot/board/domain/Article.java
@@ -1,15 +1,44 @@
 package com.boot.board.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
 import java.time.LocalDateTime;
 
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@Entity
 public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // auto increment (MySQL)
     private Long id;
+
+    @Setter
     private String title;  // 제목
+    @Setter
     private String content;  // 본문
+    @Setter
     private String hashtag;  // 해쉬태그
 
+    @CreatedDate
     private LocalDateTime createdAt;  // 생성일시
+    @CreatedBy
     private String createdBy;  // 생성자
+    @LastModifiedDate
     private LocalDateTime modifiedAt;  // 수정일시
+    @LastModifiedBy
     private String modifiedBy;  // 수정자
 }
